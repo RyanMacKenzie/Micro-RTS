04d096a1fa2dde24ef46173a84ff62446a33b731
Paul Wilson <pjw6900@rit.edu>
2018-11-30 10:41:31 -0500

    Mostly Working lobby ui

---
+++ Micro-RTS/Assets/LobbyManagerUIHookup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyManagerUIHookup : NetworkLobbyManager {
+
+    [SerializeField] GameObject NetworkLobbyManager;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Micro-RTS/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	
+	public void LoadLobby () {
+        SceneManager.LoadScene("lobby");
+	}
+	
+	
+	public void Quit () {
+        Application.Quit();
+	}
+}
