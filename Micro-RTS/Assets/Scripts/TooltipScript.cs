2950c71328724dfe97ba2cf1da92bca9a2d09e0c
Ryan MacKenzie <rxm5568@g.rit.edu>
2018-12-03 10:44:00 -0500

    Tooltips
    Needs testing/resources per second upgrade fixing

---
+++ Micro-RTS/Assets/Scripts/TooltipScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class TooltipScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler// required interface when using the OnPointerEnter method.
+{
+    public Text tooltip;
+    //Do this when the cursor enters the rect area of this selectable UI object.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.enabled = false;
+    }
+}
